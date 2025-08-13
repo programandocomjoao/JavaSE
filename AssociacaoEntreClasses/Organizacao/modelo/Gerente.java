1df374373f9dd3b1d04a768873491e1d3ae7eb81
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-08-13 07:46:16 -0300

    Add files via upload

--- AssociacaoEntreClasses/Organizacao/modelo/Gerente.java
+++ AssociacaoEntreClasses/Organizacao/modelo/Gerente.java
@@ -1,1 +1,35 @@
+package modelo;
 
+public class Gerente {
+	private String cpf, nome;
+	private Setor setor;
+
+	public Gerente(String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Setor getSetor() {
+		return setor;
+	}
+
+	public void setSetor(Setor setor) {
+		this.setor = setor;
+	}
+}
---
+++ AssociacaoEntreClasses/Organizacao/modelo/Setor.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Setor {
+	private String sigla, descricao;
+	private Gerente gerente;
+
+	public Setor(String sigla, String descricao) {
+		this.sigla = sigla;
+		this.descricao = descricao;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Gerente getGerente() {
+		return gerente;
+	}
+
+	public void setGerente(Gerente gerente) {
+		this.gerente = gerente;
+	}
+}
