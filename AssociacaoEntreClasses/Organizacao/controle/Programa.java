00e85d7593f8150ad0bed7a93494e4f853dae4ee
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-08-13 07:45:27 -0300

    Add files via upload

--- AssociacaoEntreClasses/Organizacao/controle/Programa.java
+++ AssociacaoEntreClasses/Organizacao/controle/Programa.java
@@ -1,1 +1,33 @@
+package controle;
 
+import java.util.Scanner;
+
+import modelo.Gerente;
+import modelo.Setor;
+
+public class Programa {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("CADASTRO DO SETOR");
+		System.out.print("Digite a sigla: ");
+		String sigla = input.nextLine();
+		System.out.print("Digite a descrição: ");
+		String descricao = input.nextLine();
+		
+		System.out.println("CADASTRO DO GERENTE");
+		System.out.print("Digite o CPF: ");
+		String cpf = input.nextLine();
+		System.out.print("Digite o nome: ");
+		String nome = input.nextLine();
+		
+		Setor setor = new Setor(sigla, descricao);
+		Gerente gerente = new Gerente(cpf, nome);
+		
+		setor.setGerente(gerente);
+		gerente.setSetor(setor);
+		
+		System.out.println("O funcionário " + setor.getGerente().getNome() +
+				" é gerente do setor " + gerente.getSetor().getDescricao());
+	}
+}
