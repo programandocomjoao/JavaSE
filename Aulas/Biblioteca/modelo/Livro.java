d3fde34afdcccdef88e6a5f52552123007e676e4
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-09-04 18:21:44 -0300

    Add files via upload

--- Aulas/Biblioteca/modelo/Livro.java
+++ Aulas/Biblioteca/modelo/Livro.java
@@ -1,1 +1,18 @@
+package modelo;
 
+public class Livro {
+	private String isbn, titulo;
+	private int ano;
+	
+	public void inserir(String isbn, String titulo, int ano) {
+		this.isbn = isbn;
+		this.titulo = titulo;
+		this.ano = ano;
+	}
+	
+	public String consultar() {
+		String livro = isbn + " | " + titulo + " | " + ano;
+		
+		return livro;
+	}
+}
