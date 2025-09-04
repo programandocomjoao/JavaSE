d29b9b87ef00739f41e7b031d503a22724727185
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-09-04 18:22:36 -0300

    Add files via upload

--- Aulas/Biblioteca/controle/ControleLivro.java
+++ Aulas/Biblioteca/controle/ControleLivro.java
@@ -1,1 +1,23 @@
+package controle;
 
+import java.util.Scanner;
+import modelo.Livro;
+
+public class ControleLivro {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("CADASTRO DE LIVRO");
+		System.out.print("Digite o ISBN: ");
+		String isbn = input.nextLine();
+		System.out.print("Digite o título: ");
+		String titulo = input.nextLine();
+		System.out.print("Digite o ano: ");
+		int ano = input.nextInt();
+		
+		Livro livro = new Livro();
+		
+		livro.inserir(isbn, titulo, ano);
+		System.out.print("Livro: " + livro.consultar());
+	}
+}
