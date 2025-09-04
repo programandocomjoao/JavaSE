34e646597cb2fc4333bac90e863a9c7c008bca05
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-09-04 19:58:03 -0300

    Add files via upload

---
+++ Aulas/Biblioteca/modelo/Editora.java
@@ -0,0 +1,29 @@
+package modelo;
+
+public class Editora {
+	private String cnpj, razaoSocial, pais;
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getRazaoSocial() {
+		return razaoSocial;
+	}
+
+	public void setRazaoSocial(String razaoSocial) {
+		this.razaoSocial = razaoSocial;
+	}
+
+	public String getPais() {
+		return pais;
+	}
+
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
+}
