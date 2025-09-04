62225d47bc6f1fb85f10620e349cdadddabf3edc
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-09-04 19:41:19 -0300

    Add files via upload

---
+++ Aulas/Biblioteca/modelo/Autor.java
@@ -0,0 +1,21 @@
+package modelo;
+
+public class Autor {
+	private String nome, nacionalidade;
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+}
