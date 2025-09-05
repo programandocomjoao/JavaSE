39076d81de35bd3d32daa877ad501d1ee15bd1d0
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-09-04 21:40:20 -0300

    Add files via upload

--- Aulas/Floricultura/modelo/Compra.java
+++ Aulas/Floricultura/modelo/Compra.java
@@ -1,1 +1,34 @@
+package modelo;
 
+import java.util.Calendar;
+
+public class Compra {
+	private String data;
+	private float valor;
+	
+	public Compra() {
+		Calendar c = Calendar.getInstance();
+		
+		int dia = c.get(Calendar.DAY_OF_MONTH);
+		int mes = c.get(Calendar.MONTH) + 1;
+		int ano = c.get(Calendar.YEAR);
+		
+		data = dia + "/" + mes + "/" + ano;
+	}
+	
+	public String getData() {
+		return data;
+	}
+	
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public float getValor() {
+		return valor;
+	}
+	
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+}
