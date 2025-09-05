5a146706d68d5c0a00e6258b0f2e0b68e1e52987
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-09-04 21:41:14 -0300

    Add files via upload

--- Aulas/Floricultura/controle/ControleCompra.java
+++ Aulas/Floricultura/controle/ControleCompra.java
@@ -1,1 +1,21 @@
+package controle;
 
+import java.util.Scanner;
+
+import modelo.Compra;
+
+public class ControleCompra {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Compra compra = new Compra();
+		
+		System.out.println("REGISTRO DE NOVA COMPRA");
+		System.out.println("Data: " + compra.getData());
+		System.out.print("Digite o valor: R$ ");
+		compra.setValor(input.nextFloat());
+		
+		System.out.println("COMPRA REGISTRADA");
+		System.out.println("Data: " + compra.getData());
+		System.out.println("Valor: R$ " + compra.getValor());
+	}
+}
