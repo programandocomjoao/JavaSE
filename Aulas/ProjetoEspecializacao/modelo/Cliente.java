8c7a53c40c884ac77e02848437819090df82534d
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-10-02 21:10:26 -0300

    Add files via upload

--- Aulas/ProjetoEspecializacao/modelo/Cliente.java
+++ Aulas/ProjetoEspecializacao/modelo/Cliente.java
@@ -1,1 +1,26 @@
+package modelo;
 
+public abstract class Cliente {
+	private String endereco, telefone;
+
+	public Cliente(String endereco, String telefone) {
+		this.endereco = endereco;
+		this.telefone = telefone;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+}
---
+++ Aulas/ProjetoEspecializacao/modelo/Vendedor.java
@@ -0,0 +1,22 @@
+package modelo;
+
+public class Vendedor extends Funcionario {
+	private float comissao;
+
+	public Vendedor(String matricula, String nome, float salario, float comissao) {
+		super(matricula, nome, salario);
+		this.comissao = comissao;
+	}
+
+	public float getComissao() {
+		return comissao;
+	}
+
+	public void setComissao(float comissao) {
+		this.comissao = comissao;
+	}
+	
+	public float calcularSalario() {
+		return super.calcularSalario() + comissao;
+	}
+}
