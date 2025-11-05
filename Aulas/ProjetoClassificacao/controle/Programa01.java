a659d9e0b87f87956bd3550c62db2368742b909e
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-11-04 21:26:45 -0300

    Add files via upload

--- Aulas/ProjetoClassificacao/controle/Programa01.java
+++ Aulas/ProjetoClassificacao/controle/Programa01.java
@@ -1,1 +1,34 @@
+package controle;
 
+public class Programa01 {
+	public static int[] ordenar(int[] numeros) {
+		for(int i = 0; i < numeros.length; i++) {
+			int iMenor = i;
+			
+			for(int j = i + 1; j < numeros.length; j++)
+				if(numeros[j] < numeros[iMenor])
+					iMenor = j;
+			
+			int temp = numeros[i];
+			numeros[i] = numeros[iMenor];
+			numeros[iMenor] = temp;
+		}
+		
+		return numeros;
+	}
+	
+	public static void main(String[] args) {
+		int[] numeros = {3, 17, 26, 42, 70, 83, 1, 5, 23, 38, 68, 79, 94};
+		
+		System.out.print("Antes da ordenação: ");
+		for(int i = 0; i < numeros.length; i++)
+			System.out.print(numeros[i] + " | ");
+		
+		numeros = ordenar(numeros);
+		
+		System.out.print("\nApós a ordenação: ");
+		for(int i = 0; i < numeros.length; i++)
+			System.out.print(numeros[i] + " | ");
+	}
+
+}
