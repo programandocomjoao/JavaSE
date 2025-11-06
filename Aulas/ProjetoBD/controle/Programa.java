f697831faf323bd464c95aaba621292fa81a93d7
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-11-06 18:44:30 -0300

    Add files via upload

--- Aulas/ProjetoBD/controle/Programa.java
+++ Aulas/ProjetoBD/controle/Programa.java
@@ -1,1 +1,12 @@
+package controle;
 
+import dao.*;
+
+public class Programa {
+	public static void main(String[] args) {
+		//Conectar conectar = new Conectar();
+		//CriarBanco criarBanco = new CriarBanco();
+		//CriarTabela criarTabela = new CriarTabela();
+		Inserir inserir = new Inserir();
+	}
+}
