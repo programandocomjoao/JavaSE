fa6bf98d948c280b3b795dbbef6ef44a03801c12
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2025-11-06 21:15:54 -0300

    Add files via upload

---
+++ Aulas/SonicRPG/controle/ProgramaBD.java
@@ -0,0 +1,12 @@
+package controle;
+
+import dao.Inserir;
+
+public class ProgramaBD {
+
+	public static void main(String[] args) {
+		//CriarBanco criarBanco = new CriarBanco();
+		//CriarTabela criarTabela = new CriarTabela();
+		Inserir inserir = new Inserir();
+	}
+}
