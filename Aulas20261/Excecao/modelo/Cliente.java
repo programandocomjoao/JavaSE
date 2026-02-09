77253725b5aa13e7350f5fe32363467637b4bca8
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-02-09 19:44:49 -0300

    Add files via upload

--- Aulas20261/Excecao/modelo/Cliente.java
+++ Aulas20261/Excecao/modelo/Cliente.java
@@ -1,1 +1,26 @@
+package modelo;
 
+public class Cliente {
+	private String cpf, nome;
+
+	public Cliente(String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
