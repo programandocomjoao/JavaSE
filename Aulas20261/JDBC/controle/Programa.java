275cbf385a592be5c4895acbf4bd5e8a7725631a
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-02-23 21:38:32 -0300

    Add files via upload

--- Aulas20261/JDBC/controle/Programa.java
+++ Aulas20261/JDBC/controle/Programa.java
@@ -1,1 +1,17 @@
+package controle;
 
+import dao.*;
+
+public class Programa {
+	public static void main(String[] args) {
+		//DaoConexao daoConexao = new DaoConexao();
+		//DaoCriacaoBD daoCriacaoBD = new DaoCriacaoBD();
+		//DaoCriacaoTabela daoCriacaoTabela = new DaoCriacaoTabela();
+		//DaoInclusao1 daoInclusao1 = new DaoInclusao1();
+		//DaoInclusao2 daoInclusao2 = new DaoInclusao2();
+		//DaoAlteracao daoAlteracao = new DaoAlteracao();
+		//DaoRemocao daoRemocao = new DaoRemocao();
+		//DaoLista daoLista = new DaoLista();
+		DaoConsulta daoConsulta = new DaoConsulta();
+	}
+}
