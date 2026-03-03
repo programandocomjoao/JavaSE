2fd23664d1c1d4349c85610ab4940d7482c188cd
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-02 21:42:00 -0300

    Add files via upload

--- Aulas20261/Petshop/modelo/Animal.java
+++ Aulas20261/Petshop/modelo/Animal.java
@@ -1,1 +1,33 @@
+package modelo;
 
+public class Animal {
+	private String nome, raca, especie;
+	
+	public Animal() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRaca() {
+		return raca;
+	}
+
+	public void setRaca(String raca) {
+		this.raca = raca;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+}
