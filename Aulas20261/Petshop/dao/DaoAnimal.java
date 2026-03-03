316409605308474205ad68f7b4b17cdfb13f1a5a
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-02 21:41:18 -0300

    Add files via upload

--- Aulas20261/Petshop/dao/DaoAnimal.java
+++ Aulas20261/Petshop/dao/DaoAnimal.java
@@ -1,1 +1,25 @@
+package dao;
 
+import modelo.Animal;
+
+public class DaoAnimal {
+	private static final String url = "jdbc:mysql://localhost/petshop";
+	
+	public void inserir(Animal animal) {
+		String sql = "INSERT INTO animal (nome, especie, raca) VALUES ('" + animal.getNome() + "', '" + animal.getEspecie() + "', '" + animal.getRaca() + "')";
+	
+		System.out.println(sql);
+	}
+	
+	public void consultar() {
+		
+	}
+	
+	public void alterar() {
+		
+	}
+	
+	public void excluir() {
+		
+	}
+}
