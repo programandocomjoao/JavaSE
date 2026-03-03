9da13b7ccac3704569c667cabe77de24819ec1eb
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-02 21:42:42 -0300

    Add files via upload

--- Aulas20261/Petshop/view/CadastroAnimal.java
+++ Aulas20261/Petshop/view/CadastroAnimal.java
@@ -1,1 +1,116 @@
+package view;
 
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+import controle.ControleAnimal;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CadastroAnimal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textNome;
+	private JTextField textEspecie;
+	private JTextField textRaca;
+	private ControleAnimal controleAnimal = new ControleAnimal();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastroAnimal frame = new CadastroAnimal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CadastroAnimal() {
+		setTitle("PetSys 1.0");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("CADASTRO DE ANIMAL");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Arial", Font.BOLD, 24));
+		lblNewLabel.setBounds(10, 11, 414, 37);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("NOME:");
+		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 18));
+		lblNewLabel_1.setBounds(20, 59, 101, 37);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("ESPÉCIE:");
+		lblNewLabel_1_1.setFont(new Font("Arial", Font.BOLD, 18));
+		lblNewLabel_1_1.setBounds(20, 107, 101, 37);
+		contentPane.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("RAÇA:");
+		lblNewLabel_1_2.setFont(new Font("Arial", Font.BOLD, 18));
+		lblNewLabel_1_2.setBounds(20, 155, 101, 37);
+		contentPane.add(lblNewLabel_1_2);
+		
+		textNome = new JTextField();
+		textNome.setFont(new Font("Arial", Font.PLAIN, 18));
+		textNome.setBounds(147, 59, 204, 37);
+		contentPane.add(textNome);
+		textNome.setColumns(10);
+		
+		textEspecie = new JTextField();
+		textEspecie.setFont(new Font("Arial", Font.PLAIN, 18));
+		textEspecie.setColumns(10);
+		textEspecie.setBounds(147, 107, 204, 37);
+		contentPane.add(textEspecie);
+		
+		textRaca = new JTextField();
+		textRaca.setFont(new Font("Arial", Font.PLAIN, 18));
+		textRaca.setColumns(10);
+		textRaca.setBounds(147, 155, 204, 37);
+		contentPane.add(textRaca);
+		
+		JButton btnNewButton = new JButton("CADASTRAR");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String nome = textNome.getText();
+				String especie = textEspecie.getText();
+				String raca = textRaca.getText();
+				
+				controleAnimal.cadastrarAnimal(nome, especie, raca);
+				
+				JOptionPane.showMessageDialog(null, "Animal cadastrado com sucesso!");
+				
+				textNome.setText("");
+				textEspecie.setText("");
+				textRaca.setText("");
+			}
+		});
+		btnNewButton.setFont(new Font("Arial", Font.BOLD, 18));
+		btnNewButton.setBounds(147, 203, 204, 47);
+		contentPane.add(btnNewButton);
+
+	}
+}
