beff971b380b560dbec9a2746eac38f67bd7a76c
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-16 21:36:00 -0300

    Add files via upload

--- Aulas20261/Condominio/controle/Programa.java
+++ Aulas20261/Condominio/controle/Programa.java
@@ -1,1 +1,14 @@
+package controle;
 
+import modelo.Apartamento;
+import modelo.Vaga;
+
+public class Programa {
+	public static void main(String[] args) {
+		Apartamento apartamento = new Apartamento(7, 701);
+		Vaga vaga = new Vaga('A', 13);
+		
+		apartamento.setVaga(vaga);
+		vaga.setApartamento(apartamento);
+	}
+}
