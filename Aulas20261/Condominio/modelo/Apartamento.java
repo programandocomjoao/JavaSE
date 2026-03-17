911dda49f3c6c011d588f2143645bd3ae929ef4b
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-16 21:36:35 -0300

    Add files via upload

---
+++ Aulas20261/Condominio/modelo/Apartamento.java
@@ -0,0 +1,35 @@
+package modelo;
+
+public class Apartamento {
+	private int andar, numero;
+	private Vaga vaga;
+
+	public Apartamento(int andar, int numero) {
+		this.andar = andar;
+		this.numero = numero;
+	}
+
+	public int getAndar() {
+		return andar;
+	}
+
+	public void setAndar(int andar) {
+		this.andar = andar;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Vaga getVaga() {
+		return vaga;
+	}
+
+	public void setVaga(Vaga vaga) {
+		this.vaga = vaga;
+	}
+}
--- Aulas20261/Condominio/modelo/Vaga.java
+++ Aulas20261/Condominio/modelo/Vaga.java
@@ -1,1 +1,35 @@
+package modelo;
 
+public class Vaga {
+	private char bloco;
+	private int numero;
+	private Apartamento apartamento;
+		
+	public Vaga(char bloco, int numero) {
+		this.bloco = bloco;
+		this.numero = numero;
+	}
+	
+	public char getBloco() {
+		return bloco;
+	}
+	
+	public void setBloco(char bloco) {
+		this.bloco = bloco;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Apartamento getApartamento() {
+		return apartamento;
+	}
+
+	public void setApartamento(Apartamento apartamento) {
+		this.apartamento = apartamento;
+	}	
+}
