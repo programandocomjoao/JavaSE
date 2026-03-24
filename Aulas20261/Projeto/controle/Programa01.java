bf35b3d11a854e8f7fbeaed117309ca2f37c8461
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-23 21:32:08 -0300

    Add files via upload

--- Aulas20261/Projeto/controle/Programa01.java
+++ Aulas20261/Projeto/controle/Programa01.java
@@ -1,1 +1,37 @@
+package controle;
 
+import java.util.Scanner;
+
+import modelo.Contrato;
+import modelo.Funcionario;
+
+public class Programa01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("CADASTRO DE FUNCIONÁRIO");
+		System.out.print("Digite o CPF: ");
+		String cpf = input.nextLine();
+		System.out.print("Digite o nome: ");
+		String nome = input.nextLine();
+		
+		Funcionario funcionario = new Funcionario(cpf, nome);
+		
+		System.out.println("CADASTRO DE CONTRATO");
+		System.out.print("Digite o número: ");
+		String numero = input.nextLine();
+		System.out.print("Digite a data de início: ");
+		String inicio = input.nextLine();
+		
+		Contrato contrato = new Contrato(numero, inicio);
+		
+		funcionario.setContrato(contrato);
+		contrato.setFuncionario(funcionario);
+		
+		System.out.println("CONTRATAÇÃO CADASTRADA");
+		System.out.println("CPF: " + contrato.getFuncionario().getCpf());
+		System.out.println("Nome: " + contrato.getFuncionario().getNome());
+		System.out.println("Número: " + funcionario.getContrato().getNumero());
+		System.out.println("Início: " + funcionario.getContrato().getInicio());
+	}
+}
