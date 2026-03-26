599d9c9530f4f8bb464c37b5036712ad3b78a067
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-03-25 21:33:20 -0300

    Add files via upload

--- Aulas20261/Empreiteira/controle/Programa01.java
+++ Aulas20261/Empreiteira/controle/Programa01.java
@@ -1,1 +1,37 @@
+package controle;
 
+import java.util.Scanner;
+
+import modelo.Contrato;
+import modelo.Funcionario;
+
+public class Programa01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("CADASTRO DE FUNCIONÁRIO");
+		System.out.print("Digite o CPF: ");
+		String cpf = input.nextLine();
+		System.out.print("Digite o nome: ");
+		String nome = input.nextLine();
+		
+		Funcionario funcionario = new Funcionario(cpf, nome);
+		
+		System.out.println("CADASTRO DE CONTRATO");
+		System.out.print("Digite o número: ");
+		String numero = input.nextLine();
+		System.out.print("Digite a data de início: ");
+		String inicio = input.nextLine();
+		
+		Contrato contrato = new Contrato(numero, inicio);
+		
+		funcionario.setContrato(contrato);
+		contrato.setFuncionario(funcionario);
+		
+		System.out.println("DADOS CADASTRADOS");
+		System.out.println("CPF do funcionário: " + contrato.getFuncionario().getCpf());
+		System.out.println("Nome do funcionário: " + contrato.getFuncionario().getNome());
+		System.out.println("Número do contrato: " + funcionario.getContrato().getNumero());
+		System.out.println("Data de início: " + funcionario.getContrato().getInicio());
+	}
+}
---
+++ Aulas20261/Empreiteira/controle/Programa02.java
@@ -0,0 +1,38 @@
+package controle;
+
+import java.util.Scanner;
+
+import modelo.Funcionario;
+import modelo.Setor;
+
+public class Programa02 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("CADASTRO DE SETOR");
+		System.out.print("Digite a descrição: ");
+		String descricao = input.nextLine();
+		System.out.print("Digite o ramal: ");
+		int ramal = input.nextInt();
+		
+		Setor setor = new Setor(descricao, ramal);
+		
+		input.nextLine();
+		System.out.println("CADASTRO DOS FUNCIONÁRIOS");
+		for(int i = 0; i < 3; i++) {
+			System.out.print("Digite o CPF: ");
+			String cpf = input.nextLine();
+			System.out.print("Digite o nome: ");
+			String nome = input.nextLine();
+			
+			Funcionario funcionario = new Funcionario(cpf, nome);
+			setor.adFuncionario(funcionario);
+		}
+		
+		System.out.println("FUNCIONÁRIOS DO SETOR " + setor.getDescricao());
+		for(int i = 0; i < setor.getFuncionarios().length; i++) {
+			System.out.println(setor.getFuncionarios()[i].getCpf() + " | " +
+							   setor.getFuncionarios()[i].getNome());
+		}
+	}
+}
