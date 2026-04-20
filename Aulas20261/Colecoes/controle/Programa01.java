3dc3d83c1193e153c9c527b299e4ec04ebc1665b
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-04-20 20:14:17 -0300

    Add files via upload

--- Aulas20261/Colecoes/controle/Programa01.java
+++ Aulas20261/Colecoes/controle/Programa01.java
@@ -1,1 +1,22 @@
+package controle;
 
+import java.util.Vector;
+
+public class Programa01 {
+	public static void main(String[] args) {
+		Vector pessoa = new Vector();
+		
+		String nome = "Fernanda";
+		int idade = 13;
+		float altura = 1.57f;
+		
+		pessoa.add(nome);
+		pessoa.add(idade);
+		pessoa.add(altura);
+		
+		System.out.println("DADOS DA PESSOA");
+		System.out.println("Nome: " + pessoa.get(0));
+		System.out.println("Idade: " + pessoa.get(1));
+		System.out.println("Altura: " + pessoa.get(2));
+	}
+}
---
+++ Aulas20261/Colecoes/controle/Programa03.java
@@ -0,0 +1,48 @@
+package controle;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class Programa03 {
+	public static void main(String[] args) {
+		ArrayList<String> nomes = new ArrayList<String>();
+		
+		nomes.add("Maria");
+		nomes.add("João");
+		nomes.add("Mariana");
+		nomes.add("Gabriel");
+		nomes.add("Fernanda");
+		nomes.add("Tainá");
+		
+		System.out.println(nomes);
+		System.out.println("Tamanho da coleção: " + nomes.size());
+		//System.out.println("Primeiro elemento: " + nomes.firstElement());
+		//System.out.println("Último elemento: " + nomes.lastElement());
+		System.out.println("Terceiro elemento: " + nomes.get(2));
+		System.out.println("Primeira posição de Gabriel: " + nomes.indexOf("Gabriel"));
+		System.out.println("Última posição de Gabriel: " + nomes.lastIndexOf("Gabriel"));
+		//System.out.println("Existe Fernanda na coleção: " + nomes.contains("Fernanda"));
+		//System.out.println("Existe Dimitri na coleção: " + nomes.contains("Dimitri"));
+		
+		if(nomes.contains("Fernanda"))
+			System.out.println("Fernanda está na coleção");
+		else
+			System.out.println("Fernanda não está na coleção");
+		
+		if(nomes.contains("Dimitri"))
+			System.out.println("Dimitri está na coleção");
+		else
+			System.out.println("Dimitri não está na coleção");
+		
+		System.out.println(nomes);
+		
+		nomes.add(4, "Ísis");
+		System.out.println(nomes);
+		
+		//nomes.setElementAt("José", 3);
+		System.out.println(nomes);
+		
+		nomes.remove("José");
+		System.out.println(nomes);
+	}
+}
