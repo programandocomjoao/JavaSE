47a97afc8e7eb26ec1563c0af74a493b34e07069
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-05-13 21:24:04 -0300

    Add files via upload

--- Aulas20261/CentroUniversitario/controle/Programa01.java
+++ Aulas20261/CentroUniversitario/controle/Programa01.java
@@ -1,1 +1,33 @@
+package controle;
 
+import java.util.Scanner;
+
+import modelo.Aluno;
+
+public class Programa01 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		Aluno aluno;
+		char resposta;
+		
+		System.out.println("CADASTRO DE ALUNOS");
+		do {
+			System.out.print("Matrícula: ");
+			String matricula = input.nextLine();
+			System.out.print("Nome: ");
+			String nome = input.nextLine();
+			
+			aluno = new Aluno(matricula, nome);
+			
+			System.out.print("Deseja continuar? <S/N>: ");
+			resposta = input.next().charAt(0);
+			input.nextLine();
+		} while(resposta == 'S' || resposta == 's');
+		
+		System.out.println("Foram cadastrados " + aluno.getCont() + " alunos");
+		
+		for(int i = 0; i < aluno.getCont(); i++)
+			System.out.println(aluno.getAlunos()[i].getMatricula() + " | " +
+		                       aluno.getAlunos()[i].getNome());
+	}
+}
