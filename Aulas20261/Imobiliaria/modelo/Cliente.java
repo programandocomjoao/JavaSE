9a446dce8a745067ed283ce6cfeed436d7ed733c
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-05-18 21:02:37 -0300

    Add files via upload

--- Aulas20261/Imobiliaria/modelo/Cliente.java
+++ Aulas20261/Imobiliaria/modelo/Cliente.java
@@ -1,1 +1,69 @@
+package modelo;
 
+public class Cliente {
+	private String cpf, nome;
+	
+	public class Imovel {
+		private String rua, numero, bairro, cidade;
+
+		public Imovel(String rua, String numero, String bairro, String cidade) {
+			this.rua = rua;
+			this.numero = numero;
+			this.bairro = bairro;
+			this.cidade = cidade;
+		}
+
+		public String getRua() {
+			return rua;
+		}
+
+		public void setRua(String rua) {
+			this.rua = rua;
+		}
+
+		public String getNumero() {
+			return numero;
+		}
+
+		public void setNumero(String numero) {
+			this.numero = numero;
+		}
+
+		public String getBairro() {
+			return bairro;
+		}
+
+		public void setBairro(String bairro) {
+			this.bairro = bairro;
+		}
+
+		public String getCidade() {
+			return cidade;
+		}
+
+		public void setCidade(String cidade) {
+			this.cidade = cidade;
+		}
+	}
+
+	public Cliente(String cpf, String nome) {
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
