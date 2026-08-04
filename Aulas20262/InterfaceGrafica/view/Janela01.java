1b33759433837807e262b4712aa8086a8f1054c3
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-08-03 21:36:58 -0300

    Add files via upload

--- Aulas20262/InterfaceGrafica/view/Janela01.java
+++ Aulas20262/InterfaceGrafica/view/Janela01.java
@@ -1,1 +1,36 @@
+package view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Janela01 extends JFrame {
+	private JPanel tela;
+	
+	public Janela01() {
+		tela = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.setColor(Color.BLUE);
+				g.drawLine(0, 240, 640, 240);
+				g.drawRect(10, 25, 30, 30);
+				g.drawOval(100, 20, 140, 140);
+				
+				g.setColor(Color.RED);
+				g.drawLine(320, 0, 320, 480);
+				g.fillRect(110, 125, 130, 130);
+				g.fillOval(230, 220, 240, 240);
+				
+				g.setColor(Color.BLACK);
+				g.drawString("PONTUAÇÃO", 5, 15);
+			}
+		};
+		
+		getContentPane().add(tela);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(640, 480);
+		setVisible(true);
+		tela.repaint();
+	}
+}
