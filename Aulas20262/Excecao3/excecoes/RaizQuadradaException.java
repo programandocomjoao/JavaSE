41449ee320169adc1ff1510450f385250b375d54
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-08-17 21:37:26 -0300

    Add files via upload

--- Aulas20262/Excecao3/excecoes/RaizQuadradaException.java
+++ Aulas20262/Excecao3/excecoes/RaizQuadradaException.java
@@ -1,1 +1,9 @@
+package excecoes;
 
+public class RaizQuadradaException extends Exception {
+	private static String msgErro = "Raiz quadrada de número negativo";
+	
+	public RaizQuadradaException() {
+		super(msgErro);
+	}
+}
