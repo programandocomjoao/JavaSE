75dc3b9d70cb69d88edaa7f42639316d830423c9
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-08-17 21:39:24 -0300

    Add files via upload

--- Aulas20262/Animacao/controle/Programa01.java
+++ Aulas20262/Animacao/controle/Programa01.java
@@ -1,1 +1,10 @@
+package controle;
 
+import view.Janela01;
+
+public class Programa01 {
+	public static void main(String[] args) {
+		Janela01 janela01 = new Janela01();
+		janela01.iniciarAnimacao();
+	}
+}
---
+++ Aulas20262/Animacao/controle/Programa02.java
@@ -0,0 +1,10 @@
+package controle;
+
+import view.Janela02;
+
+public class Programa02 {
+	public static void main(String[] args) {
+		Janela02 janela02 = new Janela02();
+		janela02.iniciarAnimacao();
+	}
+}
