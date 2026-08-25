50c2522109a671ce822df65815952dd5f41d37b0
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-08-24 21:28:18 -0300

    Add files via upload

--- Aulas20262/Geometria/controle/Programa.java
+++ Aulas20262/Geometria/controle/Programa.java
@@ -1,1 +1,59 @@
+package controle;
 
+import java.util.Scanner;
+
+import modelo.Quadrado;
+import modelo.Retangulo;
+import modelo.Trapezio;
+import modelo.Triangulo;
+
+public class Programa {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("As opções de Área são:");
+		System.out.println("1.Quadrado\n2.Retângulo\n3.Triângulo\n4.Trapézio");
+		System.out.print("Digite a opção: ");
+		int opcao = input.nextInt();
+		
+		switch(opcao) {
+			case 1:
+				System.out.print("Digite o lado: ");
+				float lado = input.nextFloat();
+				
+				Quadrado quadrado = new Quadrado(lado);
+				System.out.print("Área: " + quadrado.calcularArea());
+				break;
+			case 2: 
+				System.out.print("Digite a base: ");
+				float base = input.nextFloat();
+				System.out.print("Digite a altura: ");
+				float altura = input.nextFloat();
+				
+				Retangulo retangulo = new Retangulo(base, altura);
+				System.out.print("Área: " + retangulo.calcularArea());
+				break;
+			case 3:
+				System.out.print("Digite a base: ");
+				base = input.nextFloat();
+				System.out.print("Digite a altura: ");
+				altura = input.nextFloat();
+				
+				Triangulo triangulo = new Triangulo(base, altura);
+				System.out.print("Área: " + triangulo.calcularArea());
+				break;
+			case 4:
+				System.out.print("Digite a base maior: ");
+				float bMaior = input.nextFloat();
+				System.out.print("Digite a base menor: ");
+				float bMenor = input.nextFloat();
+				System.out.print("Digite a altura: ");
+				altura = input.nextFloat();
+				
+				Trapezio trapezio = new Trapezio(bMaior, bMenor, altura);
+				System.out.print("Área: " + trapezio.calcularArea());
+				break;
+			default: System.out.println("Opção Inválida!");
+		}
+	}
+}
