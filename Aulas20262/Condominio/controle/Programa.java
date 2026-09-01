e6bc74b225a940aedde4bf0cb05d38a5968896ee
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-08-31 21:19:32 -0300

    Add files via upload

--- Aulas20262/Condominio/controle/Programa.java
+++ Aulas20262/Condominio/controle/Programa.java
@@ -1,1 +1,32 @@
+package controle;
 
+import java.util.Scanner;
+
+import modelo.Apartamento;
+import modelo.Vaga;
+
+public class Programa {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("CADASTRO DE CONDÔMINO");
+		System.out.print("Andar: ");
+		int andar = input.nextInt();
+		System.out.print("Número do apartamento: ");
+		int numAp = input.nextInt();
+		System.out.print("Bloco da vaga: ");
+		char bloco = input.next().charAt(0);
+		System.out.print("Número da vaga: ");
+		int numVaga = input.nextInt();
+		
+		Apartamento apartamento = new Apartamento(andar, numAp);
+		Vaga vaga = new Vaga(bloco, numVaga);
+		
+		apartamento.setVaga(vaga);
+		vaga.setApartamento(apartamento);
+		
+		System.out.println("CONDÔMINO CADASTRADO");
+		System.out.println("Apartamento: " + vaga.getApartamento().getNumero());
+		System.out.println("Vaga: " + apartamento.getVaga().getNumero());
+	}
+}
