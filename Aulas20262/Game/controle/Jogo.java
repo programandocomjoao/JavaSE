2e83edde334a06df2fad54982b047163f40b90d4
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-09-21 17:39:57 -0300

    Create Jogo.java

---
+++ Aulas20262/Game/controle/Jogo.java
@@ -0,0 +1,10 @@
+package controle;
+
+import view.TelaJogo;
+
+public class Jogo {
+  public static void main(String[] args) {
+    TelaJogo telaJogo = new TelaJogo();
+    telaJogo.iniciar();
+  }
+}
