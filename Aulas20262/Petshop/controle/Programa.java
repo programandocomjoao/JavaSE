ae2b50006528b1aa001f0b4f20040752eef94a64
Jota Pê <46997033+programandocomjoao@users.noreply.github.com>
2026-09-21 21:15:52 -0300

    Add files via upload

--- Aulas20262/Petshop/controle/Programa.java
+++ Aulas20262/Petshop/controle/Programa.java
@@ -1,1 +1,36 @@
+package controle;
 
+import java.util.Scanner;
+
+import modelo.Animal;
+import modelo.Cliente;
+
+public class Programa {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("CADASTRO DE CLIENTE");
+		System.out.print("CPF: ");
+		String cpf = input.nextLine();
+		System.out.print("Nome: ");
+		String nome = input.nextLine();
+		
+		Cliente cliente = new Cliente(cpf, nome);
+		
+		System.out.println("CADASTRO DE ANIMAIS");
+		for(int i = 0; i < 3; i++) {
+			System.out.println("ANIMAL " + (i+1) + ": ");
+			System.out.print("Nome: ");
+			String nomePet = input.nextLine();
+			System.out.print("Raça: ");
+			String raca = input.nextLine();
+			
+			Animal animal = new Animal(nomePet, raca);
+			cliente.adAnimal(animal);
+		}
+		
+		System.out.println("ANIMAIS DO CLIENTE " + cliente.getNome());
+		for(Animal a : cliente.getAnimais())
+			System.out.println(a.getNome() + " | " + a.getRaca());
+	}
+}
